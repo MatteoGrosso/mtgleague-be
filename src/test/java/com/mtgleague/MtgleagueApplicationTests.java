678a5eddfaa1274bd1a963707c7a42b4194366e2
Matteo <grossomatteo.lavoro@gmail.com>
2024-04-11 11:23:06 +0200

    prod

--- src/test/java/com/mtgleague/MtgleagueApplicationTests.java
+++ src/test/java/com/mtgleague/MtgleagueApplicationTests.java
@@ -1,13 +1,6 @@
 package com.mtgleague;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MtgleagueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
