348287ed78c4a45ecc99411c8c1f3ec94cdfc6e1
Matteo <grossomatteo.lavoro@gmail.com>
2024-04-17 21:11:45 +0200

    reset to local env

--- src/main/java/com/mtgleague/MtgleagueApplication.java
+++ src/main/java/com/mtgleague/MtgleagueApplication.java
@@ -1,16 +1,10 @@
 package com.mtgleague;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 
 import java.util.Arrays;
 
@@ -35,40 +29,5 @@ public class MtgleagueApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
-
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		// Enable SSL Traffic
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		// Add HTTP to HTTPS redirect
-		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-		return tomcat;
-	}
-
-	/*
-    Redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8080. With SSL it will use port 443. So, any request for 8080 needs to be
-    redirected to HTTPS on 443.
-     */
-	private Connector httpToHttpsRedirectConnector() {
-		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(443);
-		return connector;
 	}
 }
