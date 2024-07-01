875040ee1b076d3dea692de862e5d8617ed75bc8
Matteo <grossomatteo.lavoro@gmail.com>
2024-07-02 00:08:03 +0200

    updated CORS

--- src/main/java/com/mtgleague/MtgleagueApplication.java
+++ src/main/java/com/mtgleague/MtgleagueApplication.java
@@ -19,7 +19,7 @@ public class MtgleagueApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://main.d3a0aig49lr38f.amplifyapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://main.d3a0aig49lr38f.amplifyapp.com", "https://pauperleague.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
